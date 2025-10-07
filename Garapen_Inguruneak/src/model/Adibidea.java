9eb1b1913a3426f08d62ec11ce9d89dc1c1db307
aelorza-tech <aelorza@fpzornotzalh.eus>
2025-10-07 09:57:40 +0200

    Update Adibidea.java

--- Garapen_Inguruneak/src/model/Adibidea.java
+++ Garapen_Inguruneak/src/model/Adibidea.java
@@ -3,8 +3,8 @@ package model;
 public class Adibidea {
 
 	public static void main(String[] args) {
-		//Githubeko 1.froga da.
-		System.err.println("Kaixo Errorea");
+		//Githubeko 2.froga da.
+		System.err.println("Ka");
 	}
 
 }
